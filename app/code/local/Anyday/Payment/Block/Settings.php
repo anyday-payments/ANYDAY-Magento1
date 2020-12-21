029c93dc0448b63f70bc78022e536c67b104eb39
Luke Golden <74642465+luke-golden@users.noreply.github.com>
2020-12-21 13:41:04 +0700

    Bugfix API Key and Payment Logo
    [Bug] Swap API Key and Test API Key labels
    [Bug] Fix Payment Logo SVG Style

--- app/code/local/Anyday/Payment/Block/Settings.php
+++ app/code/local/Anyday/Payment/Block/Settings.php
@@ -8,6 +8,7 @@ class Anyday_Payment_Block_Settings extends Mage_Core_Block_Template
     {
         $retArray = [];
         $retArray['script']         = Mage::helper('adpayment/config')->getLinkJsFile();
+        $retArray['tagcode']        = Mage::helper('adpayment/config')->getTagToken();
         return Mage::helper('core')->jsonEncode($retArray);
     }
 }
